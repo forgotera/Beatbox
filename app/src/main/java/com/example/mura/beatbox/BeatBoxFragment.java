83a82b65e4d6ff669e20ec9223e78e53f2a70c78
Gaipov Murad <m.gaipov@mail.ru>
2019-04-09 21:15:56 +0300

    add styles

--- app/src/main/java/com/example/mura/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/example/mura/beatbox/BeatBoxFragment.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 
 import com.example.mura.beatbox.databinding.FragmentBeatBoxBinding;
 import com.example.mura.beatbox.databinding.ListItemSoundBinding;
